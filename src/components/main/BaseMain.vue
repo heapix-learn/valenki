c66ba52bc1ac9d99cc41abdf28584d6d14f1fee1
SergGergalov <gergalovsergey@gmail.com>
2018-11-20 20:05:39 +0300

    create(page): created main-page view

---
+++ src/components/main/BaseMain.vue
@@ -0,0 +1,29 @@
+<template>
+			<div class="base-main">
+							<div class="base-main__page">
+									<span>Hello World!</span>
+							</div>
+			</div>
+</template>
+
+<script>
+	export default {
+		name: "BaseMain"
+	}
+</script>
+
+<style lang="scss">
+
+.base-main{
+			padding-top: 1vh;
+			&__page {
+						background-color: aquamarine;
+						text-align: center;
+						height: 75vh;
+						padding-top: 2vh;
+						margin-bottom: 1vh;
+			}
+}
+
+
+</style>
