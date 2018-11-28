116b5fbcf61c84499058a5c221de21c218d2997d
Unknown <pashamironow@gmail.com>
2018-11-28 17:31:11 +0300

    fix (json server): Send user on successfull login

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const userdb = JSON.parse(fs.readFileSync('users.json', 'UTF-8'))
+const userdb = JSON.parse(fs.readFileSync('db.json', 'UTF-8'))
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
 const SECRET_KEY = '123456789'
@@ -30,35 +30,39 @@ function isAuthenticated ({ login, password }) {
   return userdb.users.findIndex(user => user.login === login && user.password === password) !== -1
 }
 
+function findUser ({ login, password }) {
+  return userdb.users.find(user => user.login === login && user.password === password)
+
+}
+
 server.post('/auth/login', (req, res) => {
-  const credentials = {
-    login: req.body.credential.name,
-    password: req.body.credential.password
-  }
-  if (isAuthenticated(credentials) === false) {
+  const login = req.body.credential.name
+  const password = req.body.credential.password
+  if (isAuthenticated({ login, password }) === false) {
     const status = 401
     const message = 'Incorrect login or password'
-    res.status(status).json({status, message})
+    res.status(status).json({ status, message })
     return
   }
-  const access_token = createToken(credentials)
-  res.status(200).json({access_token})
+  const user = findUser({ login, password })
+  const access_token = createToken({ login, password })
+  res.status(200).json({ access_token, user })
 })
 
-server.use(/^(?!\/auth).*$/,  (req, res, next) => {
+server.use(/^(?!\/users)(?!\/auth).*$/, (req, res, next) => {
   if (req.headers.authorization === undefined || req.headers.authorization === 'null') {
     const status = 401
     const message = 'Bad authorization header'
-    res.status(status).json({status, message})
+    res.status(status).json({ status, message })
     return
   }
   try {
-    verifyToken(req.headers.authorization.split(' ')[0]);
+    verifyToken(req.headers.authorization.split(' ')[0])
     next()
   } catch (err) {
     const status = 401
     const message = 'Error: access_token is not valid'
-    res.status(status).json({status, message})
+    res.status(status).json({ status, message })
   }
 })
 
